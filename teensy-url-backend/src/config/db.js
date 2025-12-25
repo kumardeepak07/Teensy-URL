32ad0c9a34f6e83055b8cc0abeabacd970005e9d
kumardeepak07 <39013184+kumardeepak07@users.noreply.github.com>
2025-12-25 22:28:46 +0530

    feat: update database configuration to use environment variables for connection details

--- teensy-url-backend/src/config/db.js
+++ teensy-url-backend/src/config/db.js
@@ -1,8 +1,14 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 5432),
+  user: process.env.DB_USER,
+  password: String(process.env.DB_PASSWORD || ""),
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
